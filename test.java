a1c7fabbd7717aecd8c792811b10454392a7aa95
qxzhang <3165845957@qq.com>
2017-06-19 09:22:57 +0800

    java第一个小程序

---
+++ test.java
@@ -0,0 +1,13 @@
+class Test{ 
+
+public static void main(String []args){
+
+        System.out.println("test input main arguments");
+
+        System.out.println(args[0]+"        "+args[1]);
+
+        System.out.println("end of main");
+
+  }   
+
+}
